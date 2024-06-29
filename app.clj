bebb56d509775739868cac488142c0485e42d9db
Mohamed Elpannann <152228688+momoelpannann@users.noreply.github.com>
2024-06-28 23:42:47 -0400

    Update app.clj

--- app.clj
+++ app.clj
@@ -1,11 +1,12 @@
-;; app.clj
+;;Mohamed Elpannann 40251343
+;; Assignment 3 348 Summer 2024
 ;; This is the main entry point of the program. It loads the city data from the "cities.txt" file
 ;; and starts the main menu interface by calling the `main-menu` function from the `menu` namespace.
-
 (ns app
   (:require [db]
             [menu]))
 
 ;; Load the city data and start the main menu.
-(let [cities-db (db/load-data "cities.txt")]
-  (menu/main-menu cities-db))
+(def cities-db (db/load-data "cities.txt"))
+
+(menu/main-menu cities-db)
